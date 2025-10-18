02d6ba64aed72ac20ff5c97b6bfe10593a4debe9
sbt-x <236593089+sbt-x@users.noreply.github.com>
2025-10-19 00:03:03 +0900

    👍 prismaをNestJSに導入する

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,12 +5,15 @@ import { HealthCheckModule } from "./healthcheck/healthcheck.module";
 import { RoomsModule } from "./rooms/rooms.module";
 import { TokenModule } from "./token/token.module";
 import { ConfigModule } from "@nestjs/config";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: [".env.local"],
     }),
+    PrismaModule,
     UsersModule,
     AuthModule,
     HealthCheckModule,
